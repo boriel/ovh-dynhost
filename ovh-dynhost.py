d2f0576a598620727daaf4d10830ae722b76e636
Jan Henke <Jan.Henke@taujhe.de>
2015-04-26 12:41:52 +0200

    Add skeleton ovh-dynhost.py

---
+++ ovh-dynhost.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python3
+"""
+A simple script to update the OVH DynHost entry with the current external IP of this machine.
+"""
+
+import argparse
+import ipgetter
+
+def main():
+        pass
+
+if __name__ == '__main__':
+        main()
