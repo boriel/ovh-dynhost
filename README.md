52d2e8b46c50215f92c275c1baaa0ca24013327b
Jan Henke <Jan.Henke@taujhe.de>
2015-04-26 13:43:28 +0200

    Basic structre works. Needs argument parsing and result handling.

--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 A python script to update the the IP address at OVH DynHost
 
 ## Dependecies
-This project needs the [ipgetter](https://github.com/phoemur/ipgetter) library.
+This project needs the [ipgetter](https://github.com/phoemur/ipgetter) and [requests](http://requests.readthedocs.org/en/latest/) libraries.
 
 Install with `pip`
 
 ```
-sudo pip install ipgetter
+sudo pip install ipgetter requests
 ```
--- ovh-dynhost.py
+++ ovh-dynhost.py
@@ -5,9 +5,25 @@ A simple script to update the OVH DynHost entry with the current external IP of 
 
 import argparse
 import ipgetter
+import requests
 
 def main():
-        pass
+
+	domain = ''
+	myip = ipgetter.myip()
+	user=''
+	password=''
+	
+	parser = argparse.ArgumentParser(description='Set the OVH DynHost entry to the specified IP address.')
+
+	args = parser.parse_args()
+
+	queryArguments = {'system':'dyndns', 'hostname':domain, 'myip':myip}
+	response = requests.get("https://www.ovh.com/nic/update", params=queryArguments, auth=(user, password))
+	if response.status_code == requests.codes.ok:
+		pass
+	else:
+		pass
 
 if __name__ == '__main__':
-        main()
+	main()
