5fee75af1d951eca0d7a8fca88745c7bd901ade1
Jose Rodriguez <boriel@gmail.com>
2019-04-29 21:54:46 +0100

    Make the script more PEP-8 compliant
    Remove spaces in blank lines and follow the 4 spaces rule.

--- ovh_dynhost.py
+++ ovh_dynhost.py
@@ -10,48 +10,51 @@ import socket
 import sys
 
 def main():
-	parser = argparse.ArgumentParser(prog='ovh_dynhost', description='Updates the OVH DynHost entry to the current IP address.')
-	parser.add_argument('domain', help='The FQDN to be updated.')
-	parser.add_argument('user', help='The user id that has access to update the FQDN.')
-	parser.add_argument('password', help='The password for the user id.')
-	parser.add_argument('-ip', help='Update to this ip address instead of the current external address.', default=ipgetter.myip(), metavar='x.x.x.x')
-	args = parser.parse_args()
-	
-	domain = args.domain
-	ip = args.ip
-	user = args.user
-	password = args.password
-	
-	if not check_dns(domain, ip):
-		update_dns(domain, ip, user, password)
-	else:
-		sys.stdout.write("IP did not change.\n")
+    parser = argparse.ArgumentParser(prog='ovh_dynhost', description='Updates the OVH DynHost entry to the current IP address.')
+    parser.add_argument('domain', help='The FQDN to be updated.')
+    parser.add_argument('user', help='The user id that has access to update the FQDN.')
+    parser.add_argument('password', help='The password for the user id.')
+    parser.add_argument('-ip', help='Update to this ip address instead of the current external address.', default=ipgetter.myip(), metavar='x.x.x.x')
+    args = parser.parse_args()
+
+    domain = args.domain
+    ip = args.ip
+    user = args.user
+    password = args.password
+
+    if not check_dns(domain, ip):
+        update_dns(domain, ip, user, password)
+    else:
+        sys.stdout.write("IP did not change.\n")
 
 def check_dns(domain, ip):
-	addr_info = socket.getaddrinfo(domain, 80)
-	for i in range(len(addr_info)):
-		if addr_info[i][4][0] == ip:
-			return True
-		
-	return False
+    addr_info = socket.getaddrinfo(domain, 80)
+    for i in range(len(addr_info)):
+        if addr_info[i][4][0] == ip:
+            return True
+
+    return False
 
 def update_dns(domain, ip, user, password):
-	queryArguments = {'system':'dyndns', 'hostname':domain, 'myip':ip}
-	response = requests.get("https://www.ovh.com/nic/update", params=queryArguments, auth=(user, password))
-	if response.text.startswith('good ' + ip):
-			sys.stdout.write("Update to " + ip + " successful.\n")
-			sys.exit(0)
-	elif response.status_code == requests.codes.get(401):
-		# 401 Authentification failed
-		sys.stderr.write("Authentification failed. User or password is wrong.\n")
-		sys.exit(-2)
-	elif response.status_code == requests.codes.get(403):
-		# 403 Forbidden
-		sys.stderr.write("Authentification is missing.\n")
-		sys.exit(-3)
-	
-	sys.stderr.write("Update failed.\n")
-	sys.exit(-1)
+    queryArguments = {'system':'dyndns', 'hostname':domain, 'myip':ip}
+    response = requests.get("https://www.ovh.com/nic/update", params=queryArguments, auth=(user, password))
+    if response.text.startswith('good ' + ip):
+            sys.stdout.write("Update to " + ip + " successful.\n")
+            sys.exit(0)
+    elif response.text.startswith('nochg ' + ip):
+            sys.stdout.write("IP did not change\n")
+            sys.exit(0)
+    elif response.status_code == requests.codes.get(401):
+        # 401 Authentification failed
+        sys.stderr.write("Authentification failed. User or password is wrong.\n")
+        sys.exit(-2)
+    elif response.status_code == requests.codes.get(403):
+        # 403 Forbidden
+        sys.stderr.write("Authentification is missing.\n")
+        sys.exit(-3)
+    
+    sys.stderr.write("Update failed.\n")
+    sys.exit(-1)
 
 if __name__ == '__main__':
-	main()
+    main()
